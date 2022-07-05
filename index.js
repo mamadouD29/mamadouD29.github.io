af42fe6b6fce5c934eb72e6abeb3521521a02a34
Mamadou <91107287+mamadouD29@users.noreply.github.com>
2022-07-05 00:20:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let hbg = document.querySelector(".hamburger");
 let nvList = document.querySelector(".nav-list");
 let emdidi = document.getElementById("emdidi");
 let heroW = document.querySelector(".hero-wrapper");
+let body = document.querySelector(".body");
+
 
 let displayNavList = function (event) {
     hbg.classList.toggle("active");
@@ -12,6 +14,12 @@ let displayNavList = function (event) {
     heroW.addEventListener("click", (a)=>{
         a.preventDefault();
     })
+
+    body.addEventListener("keydown", (e)=>{
+        e.defaultPrevented();
+    })
+
+
 
 }
 
